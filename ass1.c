0ea83967a476ffa29a63f74521d17d66366233fd
Tantoh Telmah <82346268+tantohtelmah@users.noreply.github.com>
2022-02-21 17:32:46 +0100

    excercise one
    Write a shell script that uses bash interpreter which will take just one parameter, compile and run the C file that corresponds to that parameter

---
+++ ass1.c
@@ -0,0 +1,12 @@
+/*
+This small C program is to be executed at the terminal by a shell script that uses the bash interpreter.
+    shell script...
+    << cc -o ass1.c ass1 >>
+    << ./ass1 >>
+*/
+#include <stdio.h>
+int main()
+{
+printf("hello world");
+return 0;
+}
